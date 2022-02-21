4181a11851eefeece30edd4697a0d379e2f20986
Furkan Demir <frndemir@gmail.com>
2022-02-21 22:19:50 +0300

    :necktie: implement board component

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,10 +1,12 @@
-import Keyboard from "../Keyboard";
-
 import styles from "./Game.module.css";
+
+import Board from "../Board";
+import Keyboard from "../Keyboard";
 
 export default function Game() {
   return (
     <div className={styles.container}>
+      <Board />
       <Keyboard />
     </div>
   );
